6b5f509c6d913a0fa6e3e9e8854ab9492e10488c
manchedtudk <s215112@student.dtu.dk>
2023-11-26 16:40:44 +0100

    Updated the presentation to include .png figures
    The RUNME file now also renders the presentation

--- R/RUNME.R
+++ R/RUNME.R
@@ -7,3 +7,7 @@ file.rename(from = "00_all.html",
 
 file.rename(from = "00_all_files",
            to = "../results/00_all_files")
+
+setwd("~/projects/Group25_Project/doc")
+
+quarto::quarto_render("presentation.qmd")
